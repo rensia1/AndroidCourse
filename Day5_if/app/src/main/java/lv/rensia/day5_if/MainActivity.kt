73bf8dc4d44850233bec9dc545bb419df9412f29
rensia <foreverstill20@gmail.com>
2020-11-26 20:09:49 +0200

    initial commit

---
+++ Day5_if/app/src/main/java/lv/rensia/day5_if/MainActivity.kt
@@ -0,0 +1,36 @@
+package lv.rensia.day5_if
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editText = findViewById<EditText>(R.id.editText)
+        val btnShowCurrent = findViewById<Button>(R.id.btnShowCurrent)
+        val btnShowPrevious = findViewById<Button>(R.id.btnShowPrevious)
+
+        var previousValue :String = ""
+        var currentValue: String
+
+        previousValue = editText.text.toString()
+
+        btnShowCurrent.setOnClickListener {
+            currentValue = editText.text.toString()
+            Toast.makeText(this, currentValue, Toast.LENGTH_SHORT).show()
+
+        }
+        btnShowPrevious.setOnClickListener {
+
+            Toast.makeText(this, previousValue.reversed(), Toast.LENGTH_SHORT).show()
+
+
+        }
+    }
+}
